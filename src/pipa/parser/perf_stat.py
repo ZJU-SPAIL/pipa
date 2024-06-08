e01f7801817d610dc38c8e55d347894cd16823ce
bernard jiang <bernardjiang5@outlook.com>
2024-06-08 20:54:07 +0800

    feat: calculate CPI based on the perf-stat data

--- src/pipa/parser/perf_stat.py
+++ src/pipa/parser/perf_stat.py
@@ -2,6 +2,64 @@ import pandas as pd
 from pandarallel import pandarallel
 
 
+class PerfStatData:
+    def __init__(self, perf_stat_csv_path: str):
+        self.data = parse_perf_stat_file(perf_stat_csv_path)
+
+    def get_CPI(self):
+        """
+        Returns the CPI (Cycles Per Instruction) data.
+
+        Returns:
+            pd.DataFrame: Dataframe containing the CPI data.
+        """
+        return (
+            self.data[self.data["metric_type"] == "cycles"]
+            .merge(
+                self.data[self.data["metric_type"] == "instructions"],
+                on=["timestamp", "cpu_id"],
+                suffixes=("_cycles", "_instructions"),
+            )
+            .assign(CPI=lambda x: x["value_cycles"] / x["value_instructions"])
+            .drop(columns=["metric_type_cycles", "metric_type_instructions"])
+        )
+
+    def get_CPI_overall(self, data_type="thread"):
+        """
+        Calculate the overall CPI (Cycles Per Instruction) based on the given data type.
+
+        Parameters:
+        - data_type (str): The type of data to calculate CPI for. Can be "thread" or "system".
+
+        Returns:
+        - If data_type is "thread", returns a DataFrame with CPI values per thread.
+        - If data_type is "system", returns the overall CPI value for the system.
+
+        Raises:
+        - ValueError: If an invalid data type is provided.
+        """
+
+        df = self.get_CPI()
+        match data_type:
+            case "thread":
+                data_per_thread = (
+                    df[["cpu_id", "value_cycles", "value_instructions"]]
+                    .groupby("cpu_id")
+                    .sum()
+                )
+                data_per_thread["CPI"] = (
+                    data_per_thread["value_cycles"]
+                    / data_per_thread["value_instructions"]
+                )
+                return data_per_thread
+            case "system":
+                total_instructions = df["value_instructions"].sum()
+                total_cycles = df["value_cycles"].sum()
+                return total_cycles / total_instructions
+            case _:
+                raise ValueError("Invalid data type")
+
+
 def parse_perf_stat_file(stat_output_path: str):
     """
     Parse the perf stat output file and return a pandas DataFrame.
@@ -25,7 +83,7 @@ def parse_perf_stat_file(stat_output_path: str):
     •   optional unit of metric
     """
     pandarallel.initialize()
-    return pd.read_csv(
+    df = pd.read_csv(
         stat_output_path,
         skiprows=1,
         names=[
@@ -39,4 +97,18 @@ def parse_perf_stat_file(stat_output_path: str):
             "opt_value",
             "opt_unit_metric",
         ],
+    ).astype(
+        {
+            "timestamp": "float64",
+            "cpu_id": str,
+            "value": "int64",
+            "unit": str,
+            "metric_type": str,
+            "run_time(ns)": "int64",
+            "run_percentage": "float64",
+            "opt_value": "float64",
+            "opt_unit_metric": str,
+        }
     )
+    df["cpu_id"] = df["cpu_id"].str.replace("CPU", "").astype(int)
+    return df
