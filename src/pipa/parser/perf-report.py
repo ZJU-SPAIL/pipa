cb63f2da366685457122abcd101596a8b25c10e2
bernard jiang <bernardjiang5@outlook.com>
2024-06-13 20:08:32 +0800

    fix: fix perf report parsing

--- src/pipa/parser/perf-report.py
+++ src/pipa/parser/perf-report.py
@@ -1,52 +1,62 @@
-import re, multiprocessing
 import pandas as pd
 from pipa.common.logger import logger
-from pipa.export_config.cpu_config import NB_PHYSICAL_CORES
 
 
-def parse_one_line(s):
+def parse_one_line(s, lr):
     try:
         # TODO support more than two columns for the overhead percentage
         (
-            overhead_cycles,
-            overhead_insns,
+            overhead,
             command,
             shared_object,
-            execution_mode,
             symbol,
-        ) = re.match(
-            r"\s*(\d+\.\d+)%\s+(\d+\.\d+)%\s+(.+?)\s+(.+?)\s+\[(\S+)\]\s+(.*)", s
-        ).groups()
+        ) = (
+            s[lr[0][0] : lr[0][1]],
+            s[lr[1][0] : lr[1][1]],
+            s[lr[2][0] : lr[2][1]],
+            s[lr[3][0] :],
+        )
+        overhead_cycles, overhead_insns = overhead.split()
+        execution_mode = symbol.split()[0]
+        symbol = " ".join(symbol.split()[1:])
+        execution_mode = execution_mode[1]
     except Exception as e:
         logger.warning("parse failed for line: " + s + "\n with error: " + str(e))
         return None
     return (
-        float(overhead_cycles),
-        float(overhead_insns),
-        command,
-        shared_object,
+        float(overhead_cycles[:-1]),
+        float(overhead_insns[:-1]),
+        command.strip(),
+        shared_object.strip(),
         execution_mode,
-        symbol,
+        symbol.strip(),
     )
 
 
-def parse_perf_report_file(parsed_report_path, processes_num=NB_PHYSICAL_CORES // 2):
+def parse_perf_report_file(parsed_report_path):
+    lr = []
     with open(parsed_report_path, "r") as file:
-        content = [l.strip() for l in file.readlines()]
+
+        lines = file.readlines()
+        for line in lines:
+            if "......." in line:
+                a = line.strip().removeprefix("#").split()
+                for x in a:
+                    if not lr:
+                        l = line.index(x)
+                        lr.append((l, l + len(x)))
+                    else:
+                        l = line.index(x, lr[-1][1])
+                        lr.append((l, l + len(x)))
+                break
+
+    content = [l for l in lines if not l.startswith("#") and l.strip() != ""]
+
     if content is None:
         logger.info("content is None")
         return None
 
-    content = [l for l in content if not l.startswith("#") and l.strip() != ""]
-
-    if len(content) > 10**6:
-        logger.info("content is too large, will use multiprocessing")
-        pool = multiprocessing.Pool(processes=processes_num)
-        data = pool.map(parse_one_line, content)
-        pool.close()
-        pool.join()
-    else:
-        data = [parse_one_line(l) for l in content]
+    data = [parse_one_line(l, lr) for l in content]
 
     data = [d for d in data if d is not None]
 
@@ -63,17 +73,4 @@ def parse_perf_report_file(parsed_report_path, processes_num=NB_PHYSICAL_CORES /
             "execution_mode",
             "symbol",
         ],
-    )
-
-
-# TODO move this to a test file
-def test_parse_line():
-    s = "    14.09%  14.18%  db_bench         db_bench                                       [.] rocksdb::DBImpl::GetImpl"
-    assert parse_one_line(s) == (
-        "14.09",
-        "14.18",
-        "db_bench",
-        "db_bench",
-        ".",
-        "rocksdb::DBImpl::GetImpl",
     )
