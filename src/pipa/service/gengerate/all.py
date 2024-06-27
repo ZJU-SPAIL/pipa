9f98052b83799a5c9624c0822d59aa8a181f1ffd
bernard jiang <bernardjiang5@outlook.com>
2024-06-27 11:26:41 +0800

    fix: fix typos

--- src/pipa/service/gengerate/all.py
+++ src/pipa/service/gengerate/all.py
@@ -11,10 +11,10 @@ def quest_summary():
     how_to_run = questionary.select(
         "Please select the way of workload you want to run.",
         choices=[
-            "Build a script that collects global performance data.",
+            "Build scripts that collect global performance data.",
             "Build a script that collects performance data and start the workload by perf.",
             "Generate a configuration template configuration of PIPA-SHU.",
-            "Build a script based on the configuration file of PIPA-SHU.",
+            "Build scripts based on the configuration file of PIPA-SHU.",
             "Exit.",
         ],
     ).ask()
@@ -23,11 +23,11 @@ def quest_summary():
         == "Build a script that collects performance data and start the workload by perf."
     ):
         run_by_pipa()
-    elif how_to_run == "Build a script that collects global performance data.":
+    elif how_to_run == "Build scripts that collect global performance data.":
         run_by_user()
     elif how_to_run == "Generate a configuration template configuration of PIPA-SHU.":
         generate_template()
-    elif how_to_run == "Build a script based on the configuration file of PIPA-SHU.":
+    elif how_to_run == "Build scripts based on the configuration file of PIPA-SHU.":
         parse_pipashu_config()
     else:
         exit(0)
