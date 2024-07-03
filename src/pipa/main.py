40e86afb267fc4673e2eff7361e7057a0195c15c
bernard jiang <bernardjiang5@outlook.com>
2024-07-03 13:32:17 +0800

    feat: introduce export system configuration

--- src/pipa/main.py
+++ src/pipa/main.py
@@ -1,5 +1,6 @@
 import fire
 from pipa.service.gengerate.all import quest_summary as generate_sh
+from pipa.service.export_sys_config import run_export_config_script
 from pipa.__about__ import __version__
 from rich import print
 
@@ -7,6 +8,9 @@ from rich import print
 class PipaCLI:
     def generate(self):
         generate_sh()
+
+    def export(self):
+        run_export_config_script("/tmp/pipa-export-sysconfig.sh")
 
     def help(self):
         print("PIPA (Platform Integrated Performance Analytics)")
@@ -16,6 +20,7 @@ class PipaCLI:
         print("  pipa help")
         print("Options:")
         print("  generate  Generate the performance collection scripts")
+        print("  export    Export system configuration")
         print("  version   Show the version of PIPA")
         print("  help      Show this help message and exit")
 
