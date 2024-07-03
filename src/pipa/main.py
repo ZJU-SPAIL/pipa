87b41e7d21ebc83d27b2a814f2acf38c76770549
bernard jiang <bernardjiang5@outlook.com>
2024-07-03 11:01:13 +0800

    feat: provide pipa-version command

--- src/pipa/main.py
+++ src/pipa/main.py
@@ -1,10 +1,17 @@
 import fire
 from pipa.service.gengerate.all import quest_summary as generate_sh
+from pipa.__about__ import __version__
 
 
 class PipaCLI:
     def generate(self):
         generate_sh()
+
+    def version(self):
+        print(f"PIPA (Platform Integrated Performance Analytics) v{__version__}")
+        print("Developed by: SPAIL, ZJU")
+        print("All rights reserved.")
+        print("Licensed under the MIT License")
 
 
 def main():
