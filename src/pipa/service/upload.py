84f43804ad92e41d1f87cc1127470af5b7536714
bernard jiang <bernardjiang5@outlook.com>
2024-07-09 21:44:31 +0800

    refactor: add comment for methods

--- src/pipa/service/upload.py
+++ src/pipa/service/upload.py
@@ -51,6 +51,23 @@ def check_cores(cores):
 
 
 def quest():
+    """
+    Collects information from the user and returns a dictionary containing the collected data.
+
+    Returns:
+        dict: A dictionary containing the collected information with the following keys:
+            - 'workload': The name of the workload.
+            - 'transaction': The number of transactions.
+            - 'data_location': The location of the data collected by PIPAShu.
+            - 'cores': The threads used in the workload.
+            - 'dev': The used disk device name.
+            - 'hw_info': The hardware configuration (sockets*cores*SMT).
+            - 'sw_info': The software configuration.
+            - 'platform': The platform.
+            - 'comment': Any comments.
+            - 'pipad_addr': The PIPAD server address.
+            - 'pipad_port': The PIPAD server port.
+    """
     workload = questionary.text("What's the name of workload?").ask()
     check_workload(workload)
 
@@ -99,6 +116,19 @@ def quest():
 
 
 def build(config: dict):
+    """
+    Builds the data for upload based on the given configuration.
+
+    Args:
+        config (dict): A dictionary containing the configuration parameters.
+
+    Returns:
+        dict: A dictionary containing the built data for upload.
+
+    Raises:
+        Exception: If the required files are missing.
+
+    """
     data_dir = config["data_location"]
     perf_stat_path = os.path.join(data_dir, "perf-stat.csv")
     sar_path = os.path.join(data_dir, "sar.txt")
@@ -106,10 +136,10 @@ def build(config: dict):
 
     if not os.path.exists(perf_stat_path):
         logger.error("perf-stat.csv does not exist.")
-        raise "perf-stat.csv does not exist."
+        raise Exception("perf-stat.csv does not exist.")
     elif not os.path.exists(sar_path):
         logger.error("sar.txt does not exist.")
-        raise "sar.txt does not exist."
+        raise Exception("sar.txt does not exist.")
     elif not perf_script_path:
         logger.warning("perf-record.txt does not exist.")
         perf_script_path = None
@@ -124,6 +154,18 @@ def build(config: dict):
 
 
 def send(data: dict, addr: str = None, port: int = 50051):
+    """
+    Sends the provided data to the specified address and port using the PIPADClient.
+
+    Args:
+        data (dict): The data to be sent.
+        addr (str, optional): The address to send the data to. If not provided, the address will be retrieved from the data dictionary.
+        port (int, optional): The port to send the data to. If not provided, the default port 50051 will be used.
+
+    Returns:
+        The result of the PIPADClient's deploy method.
+
+    """
     if not addr:
         address = data["pipad_addr"]
     if not port:
@@ -190,6 +232,15 @@ def send(data: dict, addr: str = None, port: int = 50051):
 
 
 def main(config=quest()):
+    """
+    This is the main function for the upload service in the pipa project.
+
+    Args:
+        config: The configuration for the upload service. Defaults to the result of the `quest` function.
+
+    Returns:
+        None
+    """
     data = build(config)
     send(data)
 
