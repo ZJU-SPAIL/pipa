2f7246862034458709ea28ef04ff3d1f136b3042
bernard jiang <bernardjiang5@outlook.com>
2024-07-09 22:37:44 +0800

    fix: rename gprc_pb2

--- src/pipa/service/pipad/pipad_client.py
+++ src/pipa/service/pipad/pipad_client.py
@@ -74,11 +74,13 @@ from . import pipad_pb2 as pipadlib
 from . import pipad_pb2_grpc as pipadgrpc
 
 
-class PIPADClient():
-    def __init__(self, port: Optional[int] = 5051, address: Optional[str] = "[::]") -> None:
+class PIPADClient:
+    def __init__(
+        self, port: Optional[int] = 5051, address: Optional[str] = "[::]"
+    ) -> None:
         self._port = port
         self._address = address
-    
+
     def deploy(self, data: pipadlib.DeployRequest) -> None:
         """
         Deploys the performance data using the PIPADClient to server, will store them to specified database.
@@ -101,65 +103,71 @@ if __name__ == "__main__":
     logger.setLevel(level=logging.DEBUG)
     stream_handler.setLevel(level=logging.DEBUG)
     argp = argparse.ArgumentParser()
-    argp.add_argument("-a", "--address", type=str, default="127.0.0.1", help="Specify address binding")
-    argp.add_argument("-p", "--port", type=int, default=50051, help="Specify metrics port")
+    argp.add_argument(
+        "-a", "--address", type=str, default="127.0.0.1", help="Specify address binding"
+    )
+    argp.add_argument(
+        "-p", "--port", type=int, default=50051, help="Specify metrics port"
+    )
     args = argp.parse_args()
     address = getattr(args, "address")
     port = getattr(args, "port")
     client = PIPADClient(port, address)
-    client.deploy(data=pipadlib.DeployRequest(
-        transactions=7561946,
-        throughput=25171.548361957342,
-        used_threads=[36],
-        run_time=300.416402331,
-        cycles=1220032142629,
-        instructions=4084798557821,
-        cycles_per_second=4061136919.1644998,
-        instructions_per_second=13597122281.3605,
-        CPI=0.2986762077393152,
-        cycles_per_requests=161338.38335119028,
-        path_length=540178.2236769478,
-        cpu_frequency_mhz=34545.1,
-        cpu_usr=99.39,
-        cpu_nice=0.0,
-        cpu_sys=0.16,
-        cpu_iowait=0.0,
-        cpu_steal=0.0,
-        cpu_irq=0.13,
-        cpu_soft=0.0,
-        cpu_guest=0.0,
-        cpu_gnice=0.0,
-        cpu_idle=0.32,
-        cpu_util=99.68,
-        kbmemfree=783648669,
-        kbavail=784724900,
-        kbmemused=2875231,
-        percent_memused=0.36,
-        kbbuffers=268061,
-        kbcached=4379857,
-        kbcommit=3459630,
-        percent_commit=0.43,
-        kbactive=1530294,
-        kbinact=5261120,
-        kbdirty=456,
-        kbanonpg=2102529,
-        kbslab=556534,
-        kbkstack=18573,
-        kbpgtbl=39314,
-        kbvmused=454329,
-        tps=0.75,
-        rkB_s=0.0,
-        wkB_s=30.27,
-        dkB_s=0.0,
-        areq_sz=40.32,
-        aqu_sz=0.01,
-        disk_await=6.18,
-        percent_disk_util=0.43,
-        workload='rocksdb',
-        data_location='/home/xyjiang/project/rocksdb-perf/data/randomread/static/purple-openEuler/t1-rocksdb7.9.2/perf-stat-sar-less/',
-        dev='sdc',
-        hw_info='1*1*1',
-        sw_info='1 thread, RocksDB 7.9.2 build in release mode, debug_level=0, threads_num=1, db_bench with benchmark.sh',
-        platform='SPR 4510',
-        comment='',
-    ))
+    client.deploy(
+        data=pipadlib.DeployRequest(
+            transactions=7561946,
+            throughput=25171.548361957342,
+            used_threads=[36],
+            run_time=300.416402331,
+            cycles=1220032142629,
+            instructions=4084798557821,
+            cycles_per_second=4061136919.1644998,
+            instructions_per_second=13597122281.3605,
+            CPI=0.2986762077393152,
+            cycles_per_requests=161338.38335119028,
+            path_length=540178.2236769478,
+            cpu_frequency_mhz=34545.1,
+            cpu_usr=99.39,
+            cpu_nice=0.0,
+            cpu_sys=0.16,
+            cpu_iowait=0.0,
+            cpu_steal=0.0,
+            cpu_irq=0.13,
+            cpu_soft=0.0,
+            cpu_guest=0.0,
+            cpu_gnice=0.0,
+            cpu_idle=0.32,
+            cpu_util=99.68,
+            kbmemfree=783648669,
+            kbavail=784724900,
+            kbmemused=2875231,
+            percent_memused=0.36,
+            kbbuffers=268061,
+            kbcached=4379857,
+            kbcommit=3459630,
+            percent_commit=0.43,
+            kbactive=1530294,
+            kbinact=5261120,
+            kbdirty=456,
+            kbanonpg=2102529,
+            kbslab=556534,
+            kbkstack=18573,
+            kbpgtbl=39314,
+            kbvmused=454329,
+            tps=0.75,
+            rkB_s=0.0,
+            wkB_s=30.27,
+            dkB_s=0.0,
+            areq_sz=40.32,
+            aqu_sz=0.01,
+            disk_await=6.18,
+            percent_disk_util=0.43,
+            workload="rocksdb",
+            data_location="/home/xyjiang/project/rocksdb-perf/data/randomread/static/purple-openEuler/t1-rocksdb7.9.2/perf-stat-sar-less/",
+            dev="sdc",
+            hw_info="1*1*1",
+            sw_info="1 thread, RocksDB 7.9.2 build in release mode, debug_level=0, threads_num=1, db_bench with benchmark.sh",
+            platform="SPR 4510",
+            comment="",
+        )
+    )
--- src/pipa/service/pipad/pipad_server.py
+++ src/pipa/service/pipad/pipad_server.py
@@ -76,7 +76,7 @@ def value_to_sqlite_str(v: Any) -> str:
 
 class PIPADServer:
     class PIPAService(pipadgrpc.PIPADServicer):
-        def __init__(self, outer: 'PIPADServer') -> None:
+        def __init__(self, outer: "PIPADServer") -> None:
             """
             Initializes a new instance of the PIPADServer.PIPAService class.
 
@@ -88,8 +88,10 @@ class PIPADServer:
             """
             self._outer = outer
             super().__init__()
-            
-        def Deploy(self, request: pipadlib.DeployRequest, context) -> pipadlib.DeployResp:
+
+        def Deploy(
+            self, request: pipadlib.DeployRequest, context
+        ) -> pipadlib.DeployResp:
             """
             Deploy api, send your data and store it in sqlite file
 
@@ -126,9 +128,9 @@ class PIPADServer:
                 vs.append(f"{vtsqlites}")
             kvpairs.append(f"upload_time{upload_time}")
             hs_txt = f"'{hs_prefix}-{hash(tuple(kvpairs))}'"
-            kvs = ','.join(params)
-            kss = ','.join(ks)
-            vss = ','.join(vs)
+            kvs = ",".join(params)
+            kss = ",".join(ks)
+            vss = ",".join(vs)
             create_table_comm = f"CREATE TABLE IF NOT EXISTS {self._outer._table} (hash TEXT PRIMARY KEY,upload_time INTEGER,{kvs})"
             insert_table_comm = f"INSERT INTO {self._outer._table} (hash,upload_time,{kss}) VALUES ({hs_txt},{upload_time},{vss})"
             logger.debug(f"Request to Table component: {kvs}")
@@ -147,9 +149,16 @@ class PIPADServer:
                 return pipadlib.DeployResp(message=f"deploy failed. Contact admin. {e}")
             return pipadlib.DeployResp(message="deploy success")
 
-    def __init__(self, data_location: Optional[str] = "./", port: Optional[int] = 5051,
-                 address: Optional[str] = "[::]", database: str = "example.db", table: str = "example",
-                 grafana_api_k: Optional[str] = None, grafana_url: Optional[str] = None) -> None:
+    def __init__(
+        self,
+        data_location: Optional[str] = "./",
+        port: Optional[int] = 5051,
+        address: Optional[str] = "[::]",
+        database: str = "example.db",
+        table: str = "example",
+        grafana_api_k: Optional[str] = None,
+        grafana_url: Optional[str] = None,
+    ) -> None:
         """
         Initialize the PIPADServer.
 
@@ -169,33 +178,34 @@ class PIPADServer:
         self._table = table
         self._database_loc = os.path.join(data_location, database)
         if grafana_api_k is not None and grafana_url is not None:
-            self._grafana = {
-                "api_key": grafana_api_k,
-                "url": grafana_url
-            }
+            self._grafana = {"api_key": grafana_api_k, "url": grafana_url}
             new_conn = {
                 "name": database,
                 "type": "frser-sqlite-datasource",
                 "access": "proxy",
-                "jsonData": {
-                    "path": self._database_loc
-                }
+                "jsonData": {"path": self._database_loc},
             }
             headers = {
-                'Content-Type': 'application/json',
-                'Authorization': f'Bearer {grafana_api_k}'
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {grafana_api_k}",
             }
 
-            response = requests.post(f'{grafana_url}/api/datasources', headers=headers, data=json.dumps(new_conn))
+            response = requests.post(
+                f"{grafana_url}/api/datasources",
+                headers=headers,
+                data=json.dumps(new_conn),
+            )
 
             if response.status_code == 200:
                 logger.info(f"New connection {self._database_loc} added successfully")
             elif response.status_code == 409:
                 logger.debug(f"connection {self._database_loc} exists")
             else:
-                logger.error(f"Failed to add new connection {self._database_loc}: {response.status_code}")
+                logger.error(
+                    f"Failed to add new connection {self._database_loc}: {response.status_code}"
+                )
                 logger.error(response.json())
-    
+
     def serve(self) -> None:
         """
         Initiate the server, add the PIPAD service to the server, start the server, and wait for termination.
@@ -213,16 +223,36 @@ if __name__ == "__main__":
     logger.setLevel(level=logging.DEBUG)
     stream_handler.setLevel(level=logging.DEBUG)
     argp = argparse.ArgumentParser()
-    argp.add_argument("-a", "--address", type=str, default="127.0.0.1", help="Specify address binding")
-    argp.add_argument("-p", "--port", type=int, default=50051, help="Specify metrics port")
-    argp.add_argument("-d", "--database", type=str, default="example.db", help="Specify which database to store data")
-    argp.add_argument("-t", "--table", type=str, default="example", help="Specify table name")
-    argp.add_argument("-l", "--data-location", type=str, default="./", help="Specify data location")
+    argp.add_argument(
+        "-a", "--address", type=str, default="127.0.0.1", help="Specify address binding"
+    )
+    argp.add_argument(
+        "-p", "--port", type=int, default=50051, help="Specify metrics port"
+    )
+    argp.add_argument(
+        "-d",
+        "--database",
+        type=str,
+        default="example.db",
+        help="Specify which database to store data",
+    )
+    argp.add_argument(
+        "-t", "--table", type=str, default="example", help="Specify table name"
+    )
+    argp.add_argument(
+        "-l", "--data-location", type=str, default="./", help="Specify data location"
+    )
     args = argp.parse_args()
     address = getattr(args, "address")
     port = getattr(args, "port")
     database = getattr(args, "database")
     table = getattr(args, "table")
     dlocation = getattr(args, "data_location")
-    server = PIPADServer(data_location=dlocation, port=port, address=address, database=database, table=table)
+    server = PIPADServer(
+        data_location=dlocation,
+        port=port,
+        address=address,
+        database=database,
+        table=table,
+    )
     server.serve()
