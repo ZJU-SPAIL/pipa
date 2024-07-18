869e890c3ed273567506c0f12ac4acfe21ef6d2d
bernard jiang <bernardjiang5@outlook.com>
2024-07-18 11:07:55 +0800

    docs: add comments for the methods

--- src/pipa/parser/perf_report.py
+++ src/pipa/parser/perf_report.py
@@ -3,6 +3,22 @@ from pipa.common.logger import logger
 
 
 def parse_one_line(s, lr):
+    """
+    Parse a single line of a performance report.
+
+    Args:
+        s (str): The input line to parse.
+        lr (list): A list of tuples representing the start and end indices of each field in the line.
+
+    Returns:
+        tuple: A tuple containing the parsed values from the line in the following order:
+            - overhead_cycles (float): The number of overhead cycles.
+            - overhead_insns (float): The number of overhead instructions.
+            - command (str): The command associated with the line.
+            - shared_object (str): The shared object associated with the line.
+            - execution_mode (str): The execution mode associated with the line.
+            - symbol (str): The symbol associated with the line.
+    """
     try:
         # TODO support more than two columns for the overhead percentage
         (
@@ -34,9 +50,23 @@ def parse_one_line(s, lr):
 
 
 def parse_perf_report_file(parsed_report_path):
+    """
+    Parses a performance report file and returns the parsed data as a pandas DataFrame.
+
+    Args:
+        parsed_report_path (str): The path to the parsed report file.
+
+    Returns:
+        pandas.DataFrame: The parsed data as a DataFrame with the following columns:
+            - overhead_cycles: The number of overhead cycles.
+            - overhead_insns: The number of overhead instructions.
+            - command: The command associated with the performance data.
+            - shared_object: The shared object associated with the performance data.
+            - execution_mode: The execution mode associated with the performance data.
+            - symbol: The symbol associated with the performance data.
+    """
     lr = []
     with open(parsed_report_path, "r") as file:
-
         lines = file.readlines()
         for line in lines:
             if "......." in line:
