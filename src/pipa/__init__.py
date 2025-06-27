40ba79cff7fecc8ca81251f0ed8cb4e337a2ed14
Bernard Jiang <bernardjiang5@outlook.com>
2025-06-27 11:53:49 +0800

    docs: update pipa description

--- src/pipa/__init__.py
+++ src/pipa/__init__.py
@@ -1,9 +1,8 @@
 r"""
 # What is PIPA?
 
-PIPA (Platform Integrated Performance Analytics) is a platform that aggregates a complete toolchain of performance data collection, processing, and analysis.
+PIPA (Progressive & Intelligent Performance Analytics) is a platform that aggregates a complete toolchain of performance data collection, processing, and analysis with
+ advanced algorithms, enabling users to effortlessly obtain in - depth insights into the performance of their systems and applications. It bridges the gap between raw performance data and actionable information, allowing for quick identification of bottlenecks and optimization opportunities.
 
 PIPA (枇杷, loquat) is a local fruit of Zhejiang, China. PIPA consists of three parts: loquat tree, flower and fruit, which represent the collecting & processing, analysis and conclusion of performance data respectively.
-
-PIPA is still in the active development process, and the current development focus is on the loquat tree.
 """
